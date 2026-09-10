8acea5ff4ab6398ee0a415ec3fd45817a42f7285
Ken Cheng <kencheng.kc8@gmail.com>
2026-09-09 23:25:09 -0400

    Preserve OOS signal-study compatibility helper

--- src/fdre/research/signal_study.py
+++ src/fdre/research/signal_study.py
@@ -5,4 +5,5 @@ from fdre.research.signals.study import *  # noqa: F403
 
 _apply_benjamini_hochberg = _study._apply_benjamini_hochberg
 _spearman = _study._spearman
+_split_quantiles = _study._split_quantiles
 _summarize_window = _study._summarize_window
