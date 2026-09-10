8dddc9a5e85bf68a74f47999e102f15375680216
Ken Cheng <kencheng.kc8@gmail.com>
2026-09-09 23:03:33 -0400

    Preserve private HU adjudication compatibility

--- src/fdre/research/historical_universe_identity_adjudication.py
+++ src/fdre/research/historical_universe_identity_adjudication.py
@@ -1,3 +1,10 @@
 """Compatibility import for the historical-universe package move."""
 
 from fdre.research.historical_universe.identity_adjudication import *  # noqa: F403
+from fdre.research.historical_universe.identity_adjudication import (
+    _hash as _hash,
+    _target_map as _target_map,
+    _validate_case_shape as _validate_case_shape,
+    _validate_final_identity_intervals as _validate_final_identity_intervals,
+    _validate_topology_payload as _validate_topology_payload,
+)
