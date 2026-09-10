9ef4baa6a3536c0c650e4728421907887cb2d2d7
Ken Cheng <kencheng.kc8@gmail.com>
2026-09-09 23:04:26 -0400

    Keep private HU compatibility through canonical module

--- src/fdre/research/historical_universe_identity_adjudication.py
+++ src/fdre/research/historical_universe_identity_adjudication.py
@@ -1,10 +1,10 @@
 """Compatibility import for the historical-universe package move."""
 
+from fdre.research.historical_universe import identity_adjudication as _canonical
 from fdre.research.historical_universe.identity_adjudication import *  # noqa: F403
-from fdre.research.historical_universe.identity_adjudication import (
-    _hash as _hash,
-    _target_map as _target_map,
-    _validate_case_shape as _validate_case_shape,
-    _validate_final_identity_intervals as _validate_final_identity_intervals,
-    _validate_topology_payload as _validate_topology_payload,
-)
+
+_hash = _canonical._hash
+_target_map = _canonical._target_map
+_validate_case_shape = _canonical._validate_case_shape
+_validate_final_identity_intervals = _canonical._validate_final_identity_intervals
+_validate_topology_payload = _canonical._validate_topology_payload
